43d83a9020357229bd40c3f3e9d14f8c5a76be66
aayushbaluni <73417844+aayushbaluni@users.noreply.github.com>
2021-12-12 10:35:11 +0530

    Create First_CryptoCoin.sol

---
+++ First_CryptoCoin.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract MyFirstCoin{
+    address  admin;
+    mapping(address=>uint) balance;
+   
+   //Succesfully sent!
+    event Sent(address from,address to,uint amount);
+    constructor(){
+        admin=msg.sender;
+    }
+    //send from admin
+    function SendfromAdmin(address to,uint amount) public {
+        require(admin==msg.sender);
+        balance[to] +=amount;
+    }
+
+    //if error occured
+    error InsuficientBalace(address from,address to,uint amount);
+       
+    //p2p
+    function Send(address to,uint amount) public  {
+        if(balance[msg.sender]<amount) revert InsuficientBalace(msg.sender,to,amount);
+        balance[msg.sender] -=amount;
+        balance[to] +=amount; 
+        emit Sent(msg.sender,to,amount);
+    }
+}

117d2600a4a926389e5d8b06165f9da75d5b088a
aayushbaluni <73417844+aayushbaluni@users.noreply.github.com>
2021-12-12 18:17:36 +0530

    Change Owner and result

--- Change_Owner.sol
+++ Change_Owner.sol
@@ -1,16 +1,36 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Restricted{
-address owner= msg.sender;
+contract Restricted {
+    address owner = msg.sender;
+    uint256 public time = block.timestamp;
 
-modifier OnlyOwner( address _address){
-    require(_address==msg.sender,'User Not Authorised!');
-    _;
-}
-function SetOwner(address _account) public {
-    owner=_account;
-}
-function ChangeAddress(address _account) public OnlyOwner(owner){
-    owner=_account;
-} 
+    modifier OnlyOwner(address _address) {
+        require(_address == msg.sender, "User Not Authorised!");
+        _;
+    }
+    modifier OnlyAfter(uint256 _time) {
+        require(block.timestamp == _time, "Too Early");
+        _;
+    }
+
+    modifier cost(uint256 amount) {
+        require(msg.value >= amount, "Insufficient Amount");
+        _;
+    }
+
+    function SetOwner(address _account) public {
+        owner = _account;
+    }
+
+    function ChangeAddress(address _account) public OnlyOwner(owner) {
+        owner = _account;
+    }
+
+    function DelOwner() public OnlyOwner(owner) OnlyAfter(time + 5 seconds) {
+        delete owner;
+    }
+
+    function forceChange(address _address) public payable cost(20000 ether) {
+        owner = _address;
+    }
 }
